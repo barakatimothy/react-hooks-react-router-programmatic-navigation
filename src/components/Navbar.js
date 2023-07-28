76b288c6f6f2a7e521ce04e0d37e5e18e9b13a42
barakatimothy <timothybaraka39@gmail.com>
2023-07-28 09:50:24 -0400

    Complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,7 @@ function Navbar({ setIsLoggedIn }) {
         style={linkStyles}
         activeStyle={{
           background: "darkblue",
-        }}
+       }}
       >
         Login
       </NavLink>
